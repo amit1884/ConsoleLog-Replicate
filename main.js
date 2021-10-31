f36ab1a50f61f24f03511f4f848991407375c93c
amit1884 <amitraj200raj@gmail.com>
2021-10-31 19:28:41 +0530

    left responsive sidebar added

--- main.js
+++ main.js
@@ -10,5 +10,37 @@ function showSidebar() {
     element.classList.remove("show-sidebar");
   }
 
-
+  function showLeftSidebar() {
+    var element = document.getElementById("left-sidebar");
+    element.classList.toggle("show-left-sidebar");
+  }
  
+  // Toggling the submenu of left sidebar
+function toggleProMenu(){
+  var element = document.getElementById("sm-1");
+  element.classList.toggle("sub-menu-1");
+  var element = document.getElementById("sm-2");
+  element.classList.add("sub-menu-2");
+  var element = document.getElementById("sm-3");
+  element.classList.add("sub-menu-3");
+}
+function toggleICMenu(){
+  var element = document.getElementById("sm-2");
+  element.classList.toggle("sub-menu-2");
+
+  var element = document.getElementById("sm-1");
+  element.classList.add("sub-menu-1");
+
+  var element = document.getElementById("sm-3");
+  element.classList.add("sub-menu-3");
+}
+function toggleBlogMenu(){
+  var element = document.getElementById("sm-3");
+  element.classList.toggle("sub-menu-3");
+
+  var element = document.getElementById("sm-2");
+  element.classList.add("sub-menu-2");
+
+  var element = document.getElementById("sm-1");
+  element.classList.add("sub-menu-1")
+}
