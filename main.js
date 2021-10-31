a8af028d2c9ceb6530fda376acf2cb55712a08f3
amit1884 <amitraj200raj@gmail.com>
2021-10-31 16:43:27 +0530

    all sections designed

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// function to show right sidebar
+function showSidebar() {
+    var element = document.getElementById("right-sidebar");
+    element.classList.add("show-sidebar");
+  }
+// function to hide right sidebar
+  function hideSidebar() {
+    var element = document.getElementById("right-sidebar");
+    element.classList.remove("show-sidebar");
+  }
+
+
+  // handle carousel
